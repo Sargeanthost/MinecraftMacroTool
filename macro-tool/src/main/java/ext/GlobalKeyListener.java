78567e2249e50ae0374d11254d53b008af6212e8
Sargeanthost <frc4123devs@gmail.com>
2021-04-14 18:57:51 -0700

    can just type file name, still working on turn precision

--- macro-tool/src/main/java/ext/GlobalKeyListener.java
+++ macro-tool/src/main/java/ext/GlobalKeyListener.java
@@ -12,41 +12,40 @@ import org.jnativehook.keyboard.NativeKeyListener;
 
 
 public class GlobalKeyListener implements NativeKeyListener {
-	public void nativeKeyPressed(NativeKeyEvent e) {
-		//System.out.println("Key Pressed: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
+    public void nativeKeyPressed(NativeKeyEvent e) {
+        //System.out.println("Key Pressed: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
 
-		if (e.getKeyCode() == NativeKeyEvent.VC_F10) {
-				Macro.closeMacros();
-		}
-		if (e.getKeyCode() == NativeKeyEvent.VC_F9) {
-			new Macro(Macro.prevMacro);
-		}
-		if (e.getKeyCode() == NativeKeyEvent.VC_F12) {
-			Config.readConfig();
-		}
-	}
+        if (e.getKeyCode() == NativeKeyEvent.VC_F10) {
+            Macro.closeMacros();
+        }
+        if (e.getKeyCode() == NativeKeyEvent.VC_F9) {
+            new Macro(Macro.prevMacro);
+        }
+        if (e.getKeyCode() == NativeKeyEvent.VC_F12) {
+            Config.readConfig();
+        }
+    }
 
-	public void nativeKeyReleased(NativeKeyEvent e) {
-		//System.out.println("Key Released: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
-	}
+    public void nativeKeyReleased(NativeKeyEvent e) {
+        //System.out.println("Key Released: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
+    }
 
-	public void nativeKeyTyped(NativeKeyEvent e) {
-		//System.out.println("Key Typed: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
-	}
+    public void nativeKeyTyped(NativeKeyEvent e) {
+        //System.out.println("Key Typed: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
+    }
 
-	public static void start() {
-		Logger logger = Logger.getLogger(GlobalScreen.class.getPackage().getName());
-		logger.setLevel(Level.OFF);
-		try {
-			GlobalScreen.registerNativeHook();
-		}
-		catch (NativeHookException ex) {
-			System.err.println("There was a problem registering the native hook.");
-			System.err.println(ex.getMessage());
+    public static void start() {
+        Logger logger = Logger.getLogger(GlobalScreen.class.getPackage().getName());
+        logger.setLevel(Level.OFF);
+        try {
+            GlobalScreen.registerNativeHook();
+        } catch (NativeHookException ex) {
+            System.err.println("There was a problem registering the native hook.");
+            System.err.println(ex.getMessage());
 
-			System.exit(1);
-		}
+            System.exit(1);
+        }
 
-		GlobalScreen.addNativeKeyListener(new GlobalKeyListener());
-	}
+        GlobalScreen.addNativeKeyListener(new GlobalKeyListener());
+    }
 }
--- macro-tool/src/main/java/logic/Main.java
+++ macro-tool/src/main/java/logic/Main.java
@@ -12,153 +12,171 @@ import ext.GlobalKeyListener;
 import ui.JConsole;
 
 public abstract class Main {
-	
-	static public String versionName = "Macro Parkour Tool 0.1.2";
-	static public String versionDate = "2020-05-07";
-	
-	static long millis;
-	static String[] args;
 
-	public static void main(String[] args) throws IOException, AWTException {
-		Main.args = args;
-		JConsole console = new JConsole();
-		console.setVisible(true);
-		CommandHelper ch = new CommandHelper();
-		Robot robot = new Robot();
-		GlobalKeyListener.start();
-		
-		ArrayList<Macro> mac  = new ArrayList<Macro>();
-		Config.readConfig();
+    static public String versionName = "Macro Parkour Tool 0.1.3";
+    static public String versionDate = "2021-04-13";
 
-		millis = System.currentTimeMillis();
-		millis = millis - millis % 50;
-		sleepTillTick();
-		while(true){
-				
-				ch.listen();
-				if(Macro.currentMacros.size() != 0) {
-					try {
-                        mac.addAll(Macro.currentMacros);
-						mac.forEach(Macro::readLine);
-						mac.clear();
-					} catch (ConcurrentModificationException e1) {}
-					
-					Macro.currentMacros.removeIf((Macro m) -> {
-						try {
-							return !m.userInput.ready();
-						} catch (IOException e2) { }
-						return false;
-					});
-				} 
-					if (!Macro.allOutputsClosed) {
-						outputStuff(robot);
-					} else if (Macro.justClosed){
-						for (int i = 0; i <= 8; i++) {
-							if (Helper.outputKey[i] < 1000) {
-							robot.keyRelease(Helper.outputKey[i]);
-							} else {
-								robot.mouseRelease(Helper.outputKey[i]);
-							}
-						}
-						Macro.justClosed = false;
-					}
+    static long millis;
+    static String[] args;
 
-				sleepTillTick();
-				
-				
-				millis += 50;
-		
-		}
-	}
+    public static void main(String[] args) throws AWTException {
+        Main.args = args;
+        JConsole console = new JConsole();
+        console.setVisible(true);
+        CommandHelper ch = new CommandHelper();
+        Robot robot = new Robot();
+        GlobalKeyListener.start();
 
-	
-	public static void outputStuff(Robot robot) {
-		for (int i = 0; i < 9; i++) {
-			if (Macro.pressIterations[i] > -1) {
-				Macro.pressIterations[i]--;
-				//System.out.println(logic.Helper.outputKey[i]);
-				if (Helper.outputKey[i] < 1000) {
-					if (Macro.pressIterations[i] > -1) {
-						robot.keyPress(Helper.outputKey[i]);
-					} else {
-						robot.keyRelease(Helper.outputKey[i]);}
-				} else {
-					if (Macro.pressIterations[i] > -1) {
-						robot.mousePress(Helper.outputKey[i]);
-					} else {robot.mouseRelease(Helper.outputKey[i]);}
-				}
-			}
-		}
-		
-		if (Macro.currentXRotation != Macro.xRotation) {
-			Point p = MouseInfo.getPointerInfo().getLocation();
-			int x = p.x;
-			int y = p.y;
-			
-			int fullX = calcFullX();
-			
-			int deltaX = fullX - Macro.currentXPixels;
-			Macro.currentXPixels = fullX;
-			//System.out.println(logic.Macro.currentXPixels);
-			//System.out.println(fullX);
-			//System.out.println(deltaX);
-			
-			robot.mouseMove(x+deltaX, y);
-			Macro.currentXRotation = Macro.xRotation;
-		}
-		if (Macro.currentYRotation != Macro.yRotation) {
-			Point p = MouseInfo.getPointerInfo().getLocation();
-			int x = p.x;
-			int y = p.y;
-			
-			int fullY = calcFullY();
-			
-			int deltaY = fullY - Macro.currentXPixels;
-			Macro.currentXPixels = fullY;
-			//System.out.println(logic.Macro.currentXPixels);
-			//System.out.println(fullX);
-			//System.out.println(deltaX);
-			
-			robot.mouseMove(x, y+deltaY);
-			Macro.currentXRotation = Macro.xRotation;
-		}
-		
-	}
-	
-	static int calcFullX() {
-		float num;
-		num = Macro.xRotation;
-		num = (float) (num / 0.15);
-		
-		float f = (float) (Config.mouseSensitivity * 0.6F + 0.2F);
-		float f1 = f * f * f * 8.0F;
-		int fullX = (int) (num / f1);
-		return fullX;
-			
-	}
-	static int calcFullY() {
-		float num;
-		num = Macro.yRotation;
-		num = (float) (num / 0.15);
-		
-		float f = (float) (Config.mouseSensitivity * 0.6F + 0.2F);
-		float f1 = f * f * f * 8.0F;
-		int fullY = (int) (num / f1);
-		return fullY;
-			
-	}
-	public static void sleepTillTick() {
-		//System.out.println(Arrays.toString(logic.Macro.pressIterations));
-		//System.out.println(50 - (System.currentTimeMillis() - millis));
-		
-		if (50 - (System.currentTimeMillis() - millis) > 0) {
-			try {
-				Thread.sleep((50 - (System.currentTimeMillis() - millis)));
-				//System.out.println(System.currentTimeMillis());
-			} catch (InterruptedException e3) {}
-		}
-	}
-	
+        ArrayList<Macro> mac = new ArrayList<>();
+        Config.readConfig();
+
+        millis = System.currentTimeMillis();
+        millis = millis - millis % 50;
+        sleepTillTick();
+        //noinspection InfiniteLoopStatement
+        while (true) {
+
+            ch.listen();
+            if (Macro.currentMacros.size() != 0) {
+                try {
+                    mac.addAll(Macro.currentMacros);
+                    mac.forEach(Macro::readLine);
+                    mac.clear();
+                } catch (ConcurrentModificationException e1) {
+                    e1.printStackTrace();
+                }
+
+                Macro.currentMacros.removeIf((Macro m) -> {
+                    try {
+                        return !m.userInput.ready();
+                    } catch (IOException e2) {
+                        System.out.println("main 48");
+                        e2.printStackTrace();
+                    }
+                    return false;
+                });
+            }
+            if (!Macro.allOutputsClosed) {
+                outputStuff(robot);
+            } else if (Macro.justClosed) {
+                for (int i = 0; i <= 8; i++) {
+                    if (Helper.outputKey[i] < 1000) {
+                        robot.keyRelease(Helper.outputKey[i]);
+                    } else {
+                        robot.mouseRelease(Helper.outputKey[i]);
+                    }
+                }
+                Macro.justClosed = false;
+            }
+
+            sleepTillTick();
+
+
+            millis += 50;
+
+        }
+    }
+
+
+    public static void outputStuff(Robot robot) {
+        for (int i = 0; i < 9; i++) {
+            if (Macro.pressIterations[i] > -1) {
+                Macro.pressIterations[i]--;
+                //System.out.println(logic.Helper.outputKey[i]);
+                if (Helper.outputKey[i] < 1000) {
+                    if (Macro.pressIterations[i] > -1) {
+                        robot.keyPress(Helper.outputKey[i]);
+                    } else {
+                        robot.keyRelease(Helper.outputKey[i]);
+                    }
+                } else {
+                    if (Macro.pressIterations[i] > -1) {
+                        robot.mousePress(Helper.outputKey[i]);
+                    } else {
+                        robot.mouseRelease(Helper.outputKey[i]);
+                    }
+                }
+            }
+        }
+
+        if (Macro.currentXRotation != Macro.xRotation) {
+            Point p = MouseInfo.getPointerInfo().getLocation();
+            int x = p.x;
+            int y = p.y;
+
+            int fullX = calcYaw();
+
+            int deltaX = fullX - Macro.currentXPixels;
+            Macro.currentXPixels = fullX;
+            //System.out.println(logic.Macro.currentXPixels);
+            //System.out.println(fullX);
+            //System.out.println(deltaX);
+
+            robot.mouseMove(x + deltaX, y);
+            Macro.currentXRotation = Macro.xRotation;
+        }
+        if (Macro.currentYRotation != Macro.yRotation) {
+            Point p = MouseInfo.getPointerInfo().getLocation();
+            int x = p.x;
+            int y = p.y;
+
+            int fullY = calcPitch();
+
+            int deltaY = fullY - Macro.currentXPixels;
+            Macro.currentXPixels = fullY;
+            //System.out.println(logic.Macro.currentXPixels);
+            //System.out.println(fullX);
+            //System.out.println(deltaX);
+
+            robot.mouseMove(x, y + deltaY);
+            Macro.currentXRotation = Macro.xRotation;
+        }
+
+    }
+
+    static int calcYaw() {
+        //calcfullx
+//		float num;
+//		num = Macro.xRotation;
+//		num = (float) ((double)num / 0.15);
+//
+//		float f = (float) (Config.mouseSensitivity * .6F + .2F);
+//		float f1 = f * f * f * 8.0F;
+//		return (int) (num / f);
+
+        return (int) ((45 * Math.pow(Config.mouseSensitivity * 0.6F + 0.2F, 3) * 8.0F) * 0.15D);
+// Macro.xRotation
+//		float f = (float) Math.pow(Config.mouseSensitivity * 0.6F + 0.2F, 3) * 8.0F;
+
+
+    }
+
+    static int calcPitch() {
+        //calcfully
+        float num;
+        num = Macro.yRotation;
+        num = (float) ((double) num / 0.15);
+
+        float f = (float) (Config.mouseSensitivity * 0.6F + 0.2F);
+        float f1 = f * f * f * 8.0F;
+        return (int) (num / f1);
+
+    }
+
+    public static void sleepTillTick() {
+        //System.out.println(Arrays.toString(logic.Macro.pressIterations));
+        //System.out.println(50 - (System.currentTimeMillis() - millis));
+
+        if (50 - (System.currentTimeMillis() - millis) > 0) {
+            try {
+                Thread.sleep((50 - (System.currentTimeMillis() - millis)));
+                //System.out.println(System.currentTimeMillis());
+            } catch (InterruptedException e3) {
+                e3.printStackTrace();
+            }
+        }
+    }
+
 }
 
 
