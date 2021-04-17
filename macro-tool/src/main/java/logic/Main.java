2bd096b2eae50a852930d96a91756580d6bf449b
Sargeanthost <36427968+Sargeanthost@users.noreply.github.com>
2021-04-17 14:12:50 -0700

    fixed wrong angle issue; was related to windows scaling. Ready for release.

--- macro-tool/src/main/java/logic/Main.java
+++ macro-tool/src/main/java/logic/Main.java
@@ -13,8 +13,8 @@ import ui.JConsole;
 
 public abstract class Main {
 
-    static public String versionName = "Macro Parkour Tool 0.1.4";
-    static public String versionDate = "2021-04-15";
+    static public String versionName = "Macro Parkour Tool 0.2.0";
+    static public String versionDate = "2021-04-17";
 
     static long millis;
     static String[] args;
@@ -35,7 +35,7 @@ public abstract class Main {
 
         ArrayList<Macro> mac = new ArrayList<>();
         Config.readOptions();
-
+        System.out.println("Current scaling factor: " + Helper.getScalingFactor());
         millis = System.currentTimeMillis();
         millis = millis - millis % 50;
         sleepTillTick();
@@ -112,12 +112,13 @@ public abstract class Main {
             int y = p.y;
 
             int fullX = calcYaw();
-//          now x                start x ? should be 0 the first time, try setting 45
             int deltaX = fullX - Macro.currentXPixels;
             Macro.currentXPixels = fullX;
-            //System.out.println(logic.Macro.currentXPixels);
-            //System.out.println(fullX);
-            //System.out.println(deltaX);
+
+//            System.out.println(logic.Macro.currentXPixels);
+//            System.out.println(fullX);
+//            current px to turn:
+//            System.out.println(deltaX);
 
             robot.mouseMove(x + deltaX, y);
             Macro.currentXRotation = Macro.xRotation;
@@ -131,53 +132,40 @@ public abstract class Main {
 
             int deltaY = fullY - Macro.currentYPixels;
             Macro.currentYPixels = fullY;
-            //System.out.println(logic.Macro.currentXPixels);
-            //System.out.println(fullX);
-            //System.out.println(deltaX);
+            //System.out.println(logic.Macro.currentYPixels);
+            //System.out.println(fullY);
+            //current px to turn:
+            //System.out.println(deltaY);
 
             robot.mouseMove(x, y + deltaY);
             Macro.currentYRotation = Macro.yRotation;
         }
-
     }
 
     static int calcYaw() {
-        //calcfullx
         float num;
         num = Macro.xRotation;
         num = (float) ((double) num / 0.15);
 
         float f = (float) (Config.mouseSensitivity * 0.6F + 0.2F);
         float f1 = f * f * f * 8.0F;
-
-        return (int) (num / f1);
-
-//        return (int) ((45 * Math.pow(Config.mouseSensitivity * 0.6F + 0.2F, 3) * 8.0F) * 0.15D);
-// Macro.xRotation
-
-
+        return (int) ((num / f1) / Helper.getScalingFactor());
     }
 
     static int calcPitch() {
-        //calcfully
         float num;
         num = Macro.yRotation;
         num = (float) ((double) num / 0.15);
 
         float f = (float) (Config.mouseSensitivity * 0.6F + 0.2F);
         float f1 = f * f * f * 8.0F;
-        return (int) (num / f1);
-
+        return (int) ((num / f1) / Helper.getScalingFactor());
     }
 
     public static void sleepTillTick() {
-        //System.out.println(Arrays.toString(logic.Macro.pressIterations));
-        //System.out.println(50 - (System.currentTimeMillis() - millis));
-
         if (50 - (System.currentTimeMillis() - millis) > 0) {
             try {
                 Thread.sleep((50 - (System.currentTimeMillis() - millis)));
-                //System.out.println(System.currentTimeMillis());
             } catch (InterruptedException e3) {
                 e3.printStackTrace();
             }
